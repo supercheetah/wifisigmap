95def8546a535df6df3d01a136e35467cc96a2d2
josiahbryan <josiahbryan@51f7991e-dc00-6d6d-7a35-a37c0ab28cd6>
2012-07-24 18:46:46 +0000

    Imported revised SurfaceInterpolate and added automatic 3d surface output to 'test.obj' into SigMapRenderer.cpp

--- trunk/SurfaceInterpolate.h
+++ trunk/SurfaceInterpolate.h
@@ -98,8 +98,9 @@ namespace SurfaceInterpolate
 		void setScaleValues(bool flag)		{ m_scaleValues = flag; }
 		
 		double interpolateValue(QPointF point, QList<qPointValue> inputs);
-		QList<qQuadValue> generateQuads(QList<qPointValue> points);
+		QList<qQuadValue> generateQuads(QList<qPointValue> points, bool forceGrid=false);
 		QImage renderPoints(QList<qPointValue> points, QSize renderSize = QSize(), bool renderLines=false, bool renderPointValues=false);
+		QString generate3dSurface(QList<qPointValue> points, QSizeF renderSize, double valueScale = -1);
 		
 	protected:
 		QList<qPointValue> testLine(QList<qPointValue> inputs, QPointF p1, QPointF p2, int dir);
